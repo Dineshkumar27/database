8a00768d145589672c2dffc96da2c0348a6fb5c7
Dineshkumar27 <41575761+Dineshkumar27@users.noreply.github.com>
2021-08-29 12:20:44 +0530

    Update mydb.sql

--- mydb.sql
+++ mydb.sql
@@ -1,3 +1,4 @@
 use mydb;
-create table emp1(id int(20),name varchar(20),addr varchar(20));
-insert into emp1 values(1,'XYZ','chennai');
+create table employee(id int(20),name varchar(20),addr varchar(20));
+insert into employee values(1,'XYZ','chennai');
+insert into employee values(2,'MNP','Singapore');
